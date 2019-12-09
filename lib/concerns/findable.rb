aab6ddfcb7a021aa4e5915504427d05c5748d03c
Justina Kuchta <justyna.kuchta@gmail.com>
2019-12-09 10:17:43 -0500

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,16 @@
-module Concerns::Findable
-    
-    def find_by_name(name)
-        self.all.find {|song| song.name == name}
-    end
-  
-    def find_or_create_by_name(name)
-        self.find_by_name(name) || self.create(name)
-    end
-    
-end
+# module Concerns
+#     module Findable
+        
+#         def find_by_name(name)
+#             self.all.find {|instance| instance.name == name}
+#         end
+        
+#         def find_or_create_by_name(name)
+#             if self.find_by_name(name).nil?
+#                 self.create(name)
+#             else
+#                 self.find_by_name(name)
+#             end
+#         end
+#     end
+# end
--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -7,11 +7,11 @@ class MusicImporter
     end
   
     def files
-        @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+        @files ||= Dir.glob("#{path}/*.mp3").map {|f| f.gsub("#{path}/", "") }
     end
   
     def import
-        files.each{ |f| Song.create_from_filename(f) }
+        files.each {|f| Song.create_from_filename(f)}
     end
-    
+
   end
